64e4e79f580d8316b82e16722749df705f4f8417
Luța Gheorghe <yotrevorgxg@gmail.com>
2025-06-25 12:31:11 +0300

    dashboard small change

--- src/modules/dashboard/DashboardPage.tsx
+++ src/modules/dashboard/DashboardPage.tsx
@@ -1,6 +1,6 @@
 import { LinkIcon, Settings, User, Zap } from "lucide-react";
 import { useState } from "react";
-import Layout from "../layout/Layout";
+import Footer from "../layout/components/Footer";
 
 const items = [
   { id: 1, title: "Effects", icon: Zap },
@@ -15,57 +15,61 @@ const DashboardPage = () => {
   const renderContent = () => {
     switch (selectedId) {
       case 1:
-        return <div>Content for Effects</div>;
+        return <p>Content for Effects</p>;
       case 2:
-        return <div>Content for Donation Links</div>;
+        return <p>Content for Donation Links</p>;
       case 3:
-        return <div>Content for Account</div>;
+        return <p>Content for Account</p>;
       case 4:
-        return <div>Content for Settings</div>;
+        return <p>Content for Settings</p>;
       default:
-        return <div>Select a menu item</div>;
+        return <p>Select a menu item</p>;
     }
   };
 
   return (
     <div className="bg-bg">
-      <Layout relative={false}>
-        <div className="drawer lg:drawer-open ">
-          <input id="my-drawer-2" type="checkbox" className="drawer-toggle " />
-          <div className="drawer-side ">
-            <ul className="menu bg-base-200 text-text-primary min-h-full w-80 p-4 bg-third-bg">
-              {items.map((item) => (
-                <li
-                  key={item.id}
-                  className={
-                    selectedId === item.id
-                      ? "bg-primary text-text-primary bg-second-bg rounded-xl"
-                      : ""
-                  }
+      <div className="drawer lg:drawer-open ">
+        <input id="my-drawer-2" type="checkbox" className="drawer-toggle " />
+        <div className="drawer-side ">
+          <label
+            htmlFor="my-drawer-2"
+            aria-label="close sidebar"
+            className="drawer-overlay"
+          ></label>
+          <ul className="menu bg-base-200 text-text-primary min-h-full w-80 p-4 bg-third-bg">
+            {items.map((item) => (
+              <li
+                key={item.id}
+                className={
+                  selectedId === item.id
+                    ? "bg-primary text-text-primary bg-second-bg rounded-xl"
+                    : ""
+                }
+              >
+                <a
+                  href="#!"
+                  onClick={() => setSelectedId(item.id)}
+                  className="flex items-center gap-2"
                 >
-                  <a
-                    href="#!"
-                    onClick={() => setSelectedId(item.id)}
-                    className="flex items-center gap-2"
-                  >
-                    <item.icon />
-                    {item.title}
-                  </a>
-                </li>
-              ))}
-            </ul>
-          </div>
-          <div className="drawer-content flex flex-col items-center justify-center p-4">
-            {renderContent()}
-            <label
-              htmlFor="my-drawer-2"
-              className="btn btn-primary drawer-button lg:hidden mt-4"
-            >
-              Open drawer
-            </label>
-          </div>
+                  <item.icon />
+                  {item.title}
+                </a>
+              </li>
+            ))}
+          </ul>
         </div>
-      </Layout>
+        <div className="drawer-content flex flex-col items-center justify-center p-4">
+          {renderContent()}
+          <label
+            htmlFor="my-drawer-2"
+            className="btn btn-primary drawer-button lg:hidden mt-4"
+          >
+            Open drawer
+          </label>
+        </div>
+      </div>
+      <Footer />
     </div>
   );
 };
