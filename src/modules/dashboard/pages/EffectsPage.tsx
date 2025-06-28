e36dcebf17ef40d6547956f77c98d4a8ff9df670
Luta Gheorghe <yotrevorgxg@gmail.com>
2025-06-28 07:42:25 +0300

    effects page v2

--- src/modules/dashboard/pages/EffectsPage.tsx
+++ src/modules/dashboard/pages/EffectsPage.tsx
@@ -1,9 +1,9 @@
 //TODO: aici trebuie lucrat
 const EffectsPage = () => {
   return (
-    <div className="w-full px-32 h-full">
+    <div className="w-full h-full">
       <div className="text-white text-4xl  ">Effects store</div>
-      <div className="grid grid-cols-3 p-4 space-x-8">
+      <div className="grid grid-cols-4  p-4 space-x-8">
         {StoreCard({
           title: "Tiny walkers",
           imagePath: "/new/COOL-STREAM.png",
@@ -24,9 +24,9 @@ const EffectsPage = () => {
     imagePath: string;
   }) {
     return (
-      <div className="hover:scale-110 transition-transform duration-300 ease-in-out">
+      <div className="hover:scale-110 transition-transform duration-300 ease-in-out w-[350px] ">
         <div className=" flex justify-center bg-red-300 rounded-2xl">
-          <img className="h-[200px] " src={imagePath} />
+          <img className=" h-[200px]" src={imagePath} />
         </div>
         <div className="flex justify-between py-2">
           <h1 className="text-start text-xl ">{title}</h1>
